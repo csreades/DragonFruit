4b1f1407362d73b30a58351e08e830e7e5c39d91
PaulGD03 <paulskapczyk@gmail.com>
2026-02-19 20:01:16 +0100

    fix(scene): update mouse button configuration for SceneCanvas

--- src/components/scene/SceneCanvas/SceneCanvas.tsx
+++ src/components/scene/SceneCanvas/SceneCanvas.tsx
@@ -1349,7 +1349,7 @@ export function SceneCanvas({
           onChange={handleOrbitChange}
           onEnd={handleOrbitEnd}
           target={orbitTarget}
-          mouseButtons={{ LEFT: THREE.MOUSE.ROTATE, MIDDLE: THREE.MOUSE.PAN, RIGHT: THREE.MOUSE.PAN }}
+          mouseButtons={{ LEFT: undefined as unknown as THREE.MOUSE, MIDDLE: THREE.MOUSE.ROTATE, RIGHT: THREE.MOUSE.PAN }}
         />
         <SpaceMouseController
           pivotPoint={selectedSpaceMousePivotPoint}
